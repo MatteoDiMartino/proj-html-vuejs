8772add058751e015b2b779e99d582cb4c20928e
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-23 20:16:51 +0200

    feat: add base footer

--- src/components/PageHero.vue
+++ src/components/PageHero.vue
@@ -6,6 +6,31 @@ export default {
 
 </script>
 
-<template></template>
+<template>
 
-<style scoped lang="scss"></style>
+<section>
+    <div class="container">
+        <div class="py-4 ms-box-hero">
+            <div class="text-white">
+                <h1>Professional Cycling Club</h1>
+                <span>Learn cycling from the pros.</span>
+            </div>
+            <button>ciao</button>
+        </div>
+    </div>
+</section>
+
+</template>
+
+<style scoped lang="scss">
+
+section {
+    background-image: url(../assets/img/event3.jpg);
+    background-size: cover;
+
+    .ms-box-hero {
+        height: 600px;
+    } 
+}
+
+</style>
