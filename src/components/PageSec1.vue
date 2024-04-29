f5088a2123c71cb174bdbfe7deb51ff40b20eec6
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-29 02:38:26 +0200

    feat: add bike player img

--- src/components/PageSec1.vue
+++ src/components/PageSec1.vue
@@ -12,7 +12,9 @@ export default {
 <section>
     <div class="container">
         <div class="row">
-            <div class="col-6">ciao</div>
+            <div class="col-6">
+                <img src="../assets/img/bike-player.png" class="w-75 my-5">
+            </div>
             <div class="col-6">ciao</div>
         </div>
     </div>
