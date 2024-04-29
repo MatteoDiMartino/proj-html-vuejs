2df3bcb996a92d4cb2e8302a33d0f6098046e1d4
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-29 02:35:50 +0200

    complete commit

---
+++ src/components/AppBodySection.vue
@@ -0,0 +1,24 @@
+<script>
+
+import PageSec1 from './PageSec1.vue'
+
+export default {
+    name: 'AppBodySection',
+    components: {
+        PageSec1
+    }
+}
+
+</script>
+
+<template>
+
+<PageSec1></PageSec1>
+
+</template>
+
+<style scoped lang="scss">
+
+
+
+</style>
---
+++ src/components/PageSec1.vue
@@ -0,0 +1,25 @@
+<script>
+
+export default {
+    name: 'PageSec1'
+}
+
+</script>
+
+<template>
+
+<!-- <h1>ciao</h1> -->
+
+<!-- <section>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">ciao</div>
+            <div class="col-6">ciao</div>
+        </div>
+    </div>
+</section> -->
+
+
+</template>
+
+<style scoped lang="scss"></style>
