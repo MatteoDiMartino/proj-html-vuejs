78b7736c311402ad4bc3841e92184a594b211e7e
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-29 07:14:54 +0200

    feat: add txt new component

--- src/components/PageSec2.vue
+++ src/components/PageSec2.vue
@@ -8,6 +8,14 @@ export default {
 
 <template>
 
+<div class="container">
+    <div class="row">
+        <div class="ms-box-txt text-center">
+            <div class="fw-bold fs-3">Our Professional Cycling Trainings</div>
+        </div>
+        <small class="text-center">Learn from the mountain biking expert.</small>
+    </div>
+</div>
 
 </template>
 
