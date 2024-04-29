e8b11c29425567f582c9088375e61302e9eff97a
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-29 07:26:49 +0200

    feat: add base card and style

--- src/components/PageSec2.vue
+++ src/components/PageSec2.vue
@@ -15,8 +15,51 @@ export default {
         </div>
         <small class="text-center">Learn from the mountain biking expert.</small>
     </div>
+
+    <div class="d-flex text-center justify-content-center">
+        <div class="col d-flex text-center justify-content-center">
+            <div class="ms-card-box">
+                <img src="../assets/img/training-box-1.jpg" alt="">
+            </div>
+        </div>
+        <div class="col d-flex text-center justify-content-center">
+            <div class="ms-card-box">
+                <img src="../assets/img/training-box-1.jpg" alt="">
+            </div>
+        </div>
+        <div class="col d-flex text-center justify-content-center">
+            <div class="ms-card-box">
+                <img src="../assets/img/training-box-1.jpg" alt="">
+            </div>
+        </div>
+        <div class="col d-flex text-center justify-content-center">
+            <div class="ms-card-box">
+                <img src="../assets/img/training-box-1.jpg" alt="">
+            </div>
+        </div>
+    </div>
 </div>
 
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+
+.ms-card-box {
+    margin-top: 20px;
+    border: 10px solid black;
+    height: 480px;
+    width: 270px;
+
+    col {
+        display: flex;
+        justify-content: center;
+    }
+
+    img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+    }
+}
+
+</style>
